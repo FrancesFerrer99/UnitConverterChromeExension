fd11ccf6bb3822f9cb3bcb75f6ca34e8572fdf2e
Frances <frances.ferrer1999@gmail.com>
2022-09-26 11:33:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
+
+let weightEl = document.getElementById('weight')
+let volumeEl = document.getElementById('volume')
+let lenghtEl = document.getElementById('lenght')
+let inputEl = document.getElementById('input')
+
+function convert(){
+
+}
